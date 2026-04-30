338d049994576bb536ad6991db542d6177a11644
Jeremy <Jeremy02012005@gmail.com>
2026-04-29 18:54:53 -0700

    expose stage complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   1) No, as the message fearutre might depends on multiple different components interactions to work, which is not suitable for a unit test.
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   1) yes, because it only happens under one component that can be tested in isolation with different inputs.
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,41 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  const audio = document.querySelector("audio");
+  const dropdown = document.querySelector("#horn-select");
+
+  dropdown.addEventListener("change", () => {
+    const image = document.querySelector("img");
+    image.src = `assets/images/${dropdown.value}.svg`;
+    audio.src = `assets/audio/${dropdown.value}.mp3`;
+  });
+
+  const volumeControl = document.querySelector("#volume-controls input");
+
+  volumeControl.addEventListener("input", () => {
+    const horn = document.querySelector("#volume-controls img");
+
+    if (volumeControl.value == 0) {
+      horn.src = "assets/icons/volume-level-0.svg";
+    } else if (volumeControl.value < 33) {
+      horn.src = "assets/icons/volume-level-1.svg";
+    } else if (volumeControl.value < 67) {
+      horn.src = "assets/icons/volume-level-2.svg";
+    } else {
+      horn.src = "assets/icons/volume-level-3.svg";
+    }
+    audio.volume = volumeControl.value / 100;
+  });
+
+  const playButton = document.querySelector("button");
+
+  playButton.addEventListener("click", () => {
+    audio.play();
+    if (dropdown.value == "party-horn") {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
 }
