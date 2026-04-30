1aa6c72017062338da1e7f4b013839ef39f1e3c0
Jeremy <Jeremy02012005@gmail.com>
2026-04-29 18:55:33 -0700

    explore

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -6,6 +6,40 @@ import {
   isStrongPassword,
   isDate,
   isHexColor,
-} from '../code-to-unit-test/unit-test-me';
+} from "../code-to-unit-test/unit-test-me";
 
 // TODO - Part 2
+test("isPhoneNumber", () => {
+  expect(isPhoneNumber("858-123-4567")).toBe(true);
+  expect(isPhoneNumber("(858) 123-4567")).toBe(true);
+  expect(isPhoneNumber("8581234567")).toBe(false);
+  expect(isPhoneNumber("85-12-567")).toBe(false);
+});
+
+test("isEmail", () => {
+  expect(isEmail("valid@example.com")).toBe(true);
+  expect(isEmail("jes063@ucsd.edu")).toBe(true);
+  expect(isEmail("invalid")).toBe(false);
+  expect(isEmail("another@invalid")).toBe(false);
+});
+
+test("isStrongPassword", () => {
+  expect(isStrongPassword("Valid_123")).toBe(true);
+  expect(isStrongPassword("cse110")).toBe(true);
+  expect(isStrongPassword("cse110isainterestingclass")).toBe(false);
+  expect(isStrongPassword("@invalid")).toBe(false);
+});
+
+test("isDate", () => {
+  expect(isDate("12/31/2020")).toBe(true);
+  expect(isDate("1/1/2020")).toBe(true);
+  expect(isDate("2020/12/31")).toBe(false);
+  expect(isDate("12-31-2020")).toBe(false);
+});
+
+test("isHexColor", () => {
+  expect(isHexColor("#FFF")).toBe(true);
+  expect(isHexColor("#123456")).toBe(true);
+  expect(isHexColor("1234567")).toBe(false);
+  expect(isHexColor("G12")).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,56 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
+
+function populateVoiceList() {
+  const voicelist = speechSynthesis.getVoices();
+
+  for (const voice of voicelist) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    if (voice.default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+}
 
 function init() {
-  // TODO
+  populateVoiceList();
+  const speakButton = document.querySelector("button");
+  const image = document.querySelector("img");
+  speakButton.addEventListener("click", () => {
+    const textInput = document.querySelector("#text-to-speak").value;
+    const voiceSelect = document.querySelector("#voice-select");
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    const utterance = new SpeechSynthesisUtterance(textInput);
+    const voices = speechSynthesis.getVoices();
+
+    for (const voice of voices) {
+      if (voice.name === selectedOption) {
+        utterance.voice = voice;
+        break;
+      }
+    }
+    speechSynthesis.speak(utterance);
+
+    utterance.onstart = () => {
+      image.src = "assets/images/smiling-open.png";
+    };
+    utterance.onend = () => {
+      image.src = "assets/images/smiling.png";
+    };
+  });
+}
+
+if (
+  typeof speechSynthesis !== "undefined" &&
+  speechSynthesis.onvoiceschanged !== undefined
+) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
